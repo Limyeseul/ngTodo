d2d7d9a6c12308d14898e6cdaca5647ff40287a0
USER <test@test.com>
2018-02-24 16:13:26 +0900

    관리자 화면 아코디언 메뉴 구성, 스타일 추가

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,11 +5,18 @@ import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import {RouterModule} from '@angular/router';
 import {adminRoutes} from './admin-routing';
+import {MatButtonModule, MatCardModule, MatExpansionModule, MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(adminRoutes)
+    RouterModule.forChild(adminRoutes),
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   declarations: [AdminComponent, HomeComponent, NewsComponent]
 })
