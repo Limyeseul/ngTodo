7d32bd23814ae6411f18d6eb4d9f89d2363599e8
USER <test@test.com>
2018-02-24 12:07:17 +0900

    angular 애니메이션 추가

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user.service';
 import {TodoVO} from '../domain/todo.vo';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-angular',
   templateUrl: './angular.component.html',
-  styleUrls: ['./angular.component.scss']
+  styleUrls: ['./angular.component.scss'],
+
+  /*
+    1) 애니메이션(명) 정의, 해당 애니메이션을 어디에 적용할것인가 정한다.
+    2) 상태 정의
+    3) 상태에 따른 transition 정의
+  */
+  animations: [trigger('flyInOut', [
+                  state('in', style({opacity: 1, transform: 'translate(0, 0)'})),
+                  transition('void => in', [ /* void 상태에서 in상태로 */
+                    /*A 상태(옵션), 투명하고 x방향으로 -100% 이동 */
+                    style({opacity: 0, transform: 'translate(-100%, 0)'}),
+                    animate(300/*, style을 정의할 수 있지만 void=>in에서는 state에서(16Line) 정의 되어있기 때문에 생략 가능*/)]),
+                  transition('* => void', [
+                    animate(300, style({transform: 'translate(0, -100%)', opacity: '0'}))])
+                ])
+              ]
 })
 export class AngularComponent implements OnInit {
   newTodo: TodoVO = new TodoVO(); // 할일 추가를 위한 모델 데이터
